97073e2c90aaaff8b3b0e2917572cd02d4f1938d
662006desaidhana-png <662006desaidhana@gmail.com>
2026-07-13 19:44:13 +0530

    Fix __name__

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,12 +9,12 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html ") 
+    return render_template("dashboard.html")
 
 @app.route("/report")
 def report():
-    return"<h1> Incident Report "
+    return "<h1>Incident Report</h1>"
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
