3e7aeb9c53ce7bc717885b79f391c86682b1960e
662006desaidhana-png <662006desaidhana@gmail.com>
2026-07-13 21:30:11 +0530

    Fix Render deployement

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def dashboard():
 def report():
     return "<h1>Incident Report</h1>"
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
